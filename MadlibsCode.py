96482c2e248f78097180a624b2aad20cbf1167a1
Dhunt5 <deborahjhunt@att.net>
2022-05-16 18:33:31 -0700

    choice is not int and added main logic

--- MadlibsCode.py
+++ MadlibsCode.py
@@ -2,6 +2,7 @@
 #the option of choosing btwn two stories and input lists.
 
 from ast import Break
+from ctypes.wintypes import CHAR, INT
 
 
 def Bicycles():
@@ -71,16 +72,19 @@ def News_Story():
 
     print(story_2)
 
+def main():
 #using a where loop to allow the user to choose between the two functions.
 
-#while True:
-    A= Bicycles
-    B = News_Story
-    Choose_your_Madlib = input(["Choose "+A+")'Bicycles' or " +B+ " )'News Story' [A/B]"])
-#    if Choose_your_Madlib == 'A':
-#        Bicycles
-#    elif Choose_your_Madlib == 'B':
-#        News_Story
-#    elif Choose_your_Madlib != 'A' and Choose_your_Madlib != 'B':
-#        print("Please choose A or B and press enter/return.")
 
+        A = Bicycles
+        B = News_Story
+        Choose_your_Madlib = int(input("Type your choice: 1 for 'Bicycles' or 2 'News_Story' -- "))
+        if Choose_your_Madlib == 1:
+            Bicycles
+        elif Choose_your_Madlib == 2:
+            News_Story
+        else :
+            Choose_your_Madlib = int(input("Please type A or B and press enter/return. "))
+
+if __name__ == "__main__":
+    main()
